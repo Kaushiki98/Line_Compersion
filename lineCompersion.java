4f55152e72448180c719451683282b262c4c471f
kaushiki <kaushiki.ak88@gmail.com>
2021-01-22 12:46:20 +0530

    Welcome message

---
+++ lineCompersion.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class lineCompersion{
+	public static void main(String[] args){
+   	Scanner myObj = new Scanner(System.in);
+      System.out.println(" Welcome to Line Compersion Computation ");
+	}
+}
